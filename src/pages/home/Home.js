00f25c44d20ef76bbd8729cbe7400b12f5abd3de
vitor lopes <lopesvitor959@gmail.com>
2023-07-22 14:45:25 +0100

    Fetching Data

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,20 @@
+import { useFetch } from "../../hooks/useFetch"
 import "./Home.css"
 
 
 const Home = () => {
+ 
+  const {data, isLoading, error} = useFetch("http://localhost:3000/recipes")
+
+
   return (
-    <div>Home</div>
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isLoading && <p className="loading">Loading...</p>}
+      {data && data.map(recipe=>(
+        <h2 key={recipe.id}>{recipe.title}</h2>
+      ))}
+    </div>
   )
 }
 
