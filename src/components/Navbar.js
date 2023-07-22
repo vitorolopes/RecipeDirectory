1cd9f15a939ae201929a2418891c9c5e1798ea79
vitor lopes <lopesvitor959@gmail.com>
2023-07-22 14:23:15 +0100

    Routing and <Navbar>

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <nav>
+        <Link to="/" className="brand">
+          <h1>Cooking Ninja</h1>
+        </Link>
+        <Link to="/create">
+          <h1>Create Recipe</h1>
+        </Link>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/search/Search.js
@@ -0,0 +1,10 @@
+import "./Search.css"
+
+
+const Search = () => {
+  return (
+    <div>Search</div>
+  )
+}
+
+export default Search
