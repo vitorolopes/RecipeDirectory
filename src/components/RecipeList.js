9dc3535c3a7a2de442bcae477bd45bfe7f143c65
vitor lopes <lopesvitor959@gmail.com>
2023-07-24 14:28:49 +0100

    <RecipeList>

---
+++ src/components/RecipeList.js
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom"
+import "./RecipeList.css"
+
+import React from 'react'
+
+const RecipeList = ({recipes}) => {
+  return (
+    <div className="recipe-list"> 
+                                   {/* // We just output the <RecipeList> in <Home>
+                                   // in the case where we already have data.
+                                   // So there is no need to {recipes && recipes.map() ...} */}
+      {recipes.map(recipe => (
+        <div key={recipe.id} className="card">
+            <h3>{recipe.title}</h3>
+            <p>{recipe.cookingTime} to make</p>
+            <div>{recipe.method.substring(0,100)}...</div>
+            <Link to={`/recipes/${recipe.id}`}>Cook this</Link>
+        </div>
+      
+      ))}
+    </div>
+  )
+}
+
+export default RecipeList
