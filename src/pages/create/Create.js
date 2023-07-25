db3faf281f2cce184b6ae1abc7913d341c2f7592
vitor lopes <lopesvitor959@gmail.com>
2023-07-25 11:52:26 +0100

    Adding Multiple Ingredients

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 import "./Create.css"
 
 
@@ -7,12 +7,30 @@ const Create = () => {
   const [title, setTitle] = useState("")
   const [method, setMethod] = useState("")
   const [cookingTime, setCookingTime] = useState("")
+//! HERE 2
+  const [newIngredient, setNewIngredient] = useState("")
+  const [ingredients, setIngredients] = useState([])
+//! HERE 4
+  const ingredientInput = useRef(null)
 
   const handleSubmit = (e) => { 
     e.preventDefault(
-      console.log(title, method, cookingTime)
+      console.log(title, method, cookingTime, ingredients)
     )
    }
+//! HERE 3
+   const handleAdd = (e) => { 
+    e.preventDefault()
+    const ing = newIngredient.trim()
+
+    if(ing && !ingredients.includes(ing)){
+      setIngredients(prevIngredients => [...prevIngredients, ing])
+    }
+    setNewIngredient("")
+    //! HERE 4c
+    ingredientInput.current.focus() // focus() focuses an input field so that the user, 
+                                    // can just start typing right away instead of clicking into the input to type.
+  }
 
   return (
     <div className="create">
@@ -26,6 +44,25 @@ const Create = () => {
                  required
           />
         </label>
+{/* //! HERE 1 */}
+        <label>
+          <span>Recipe ingredients:</span>
+          <div className="ingredients">
+             <input type="text"
+                 onChange={(e)=> setNewIngredient(e.target.value)}
+                 value={newIngredient}
+                 //! HERE 4b
+                 ref={ingredientInput}
+             />
+             <button onClick={handleAdd} className="btn">add</button>
+          </div>
+        
+        </label>
+{/* //! HERE 5 */}
+        <p>
+          Current ingredients: {ingredients.map(i=> <em key={i}>{i}, </em>)}
+        </p>
+
         <label>
           <span>Recipe method:</span>
           <textarea
